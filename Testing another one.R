664371618308c8fd8f9e733d4221075e2e88f0f7
Lucinbell <dylanfox1@gmail.com>
2021-06-27 14:42:44 +0900

    Another test to see

--- Testing another one.R
+++ Testing another one.R
@@ -2,3 +2,7 @@ print ("This file was created within RStudio")
 
 print ("Something seems off? Not able to push the commits")
 print ("Please make this work. I am too lazy to troubleshoot this :(")
+
+print ("somehow I got it to work? By fucking authenticating with GitHut")
+
+pring ("I feel that somewhere along the line a crucial step was missed")
